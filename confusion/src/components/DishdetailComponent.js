8a97956c708bbce84351d7f4f106600b6adedb89
Justin Thomsen <thomsenjustinr@johndeere.com>
2019-06-02 13:05:24 -0500

    adding animations

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -21,6 +21,7 @@ import { Link } from "react-router-dom";
 import { Control, Errors, LocalForm } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -127,25 +128,34 @@ class CommentForm extends Component {
 
 function RenderDish({dish}){
     return(
-        <Card>
-            <CardImg width="100%" src={ baseUrl + dish.image } alt={dish.name}></CardImg>
-            <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform in
+           transformProps={{
+               exitTransform: 'scale(0.5) translateY(-50%)'
+           }}>
+            <Card>
+                <CardImg width="100%" src={ baseUrl + dish.image } alt={dish.name}></CardImg>
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     )
 }
 
 function RenderComments({comments, postComment, dishId}){
     const comment = comments.map((comment) => {
         return(
-            <React.Fragment>
-                <ul key={comment.id} className="list-unstyled">
-                    <li>{comment.comment}</li>
-                    <li>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
-                </ul>
-            </React.Fragment>
+                    <React.Fragment>
+                <Fade in>
+
+                        <ul key={comment.id} className="list-unstyled">
+
+                            <li>{comment.comment}</li>
+                            <li>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</li>
+                        </ul>
+                </Fade>
+                    </React.Fragment>
         );
     });
 
@@ -153,7 +163,9 @@ function RenderComments({comments, postComment, dishId}){
         return (
             <React.Fragment>
                 <h4>Comments</h4>
-                <div>{comment}</div>
+                <Stagger in>
+                    <div>{comment}</div>
+                </Stagger>
                 <CommentForm dishId={dishId} postComment={postComment}/>
             </React.Fragment>
         )
